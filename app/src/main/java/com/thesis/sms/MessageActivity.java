40cc34cb3fafa789769c3bb3d479cf57150ac843
yidinghe <yidinghe2012@gmail.com>
2016-07-20 22:05:38 -0400

    1. Finished first Draft Socket and Message UI. 2. TODO debug by using hardcode key since keys generated from two phones are mismatch.

--- app/src/main/java/com/thesis/sms/MessageActivity.java
+++ app/src/main/java/com/thesis/sms/MessageActivity.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +33,7 @@ import com.thesis.security.AES;
 import com.thesis.util.CommonLibs;
 import com.thesis.util.Utils;
 
-public class MessageActivity extends BaseActivity {
+public class MessageActivity extends BaseActivity implements Runnable {
 
     // LogCat tag
     private static final String TAG = MessageActivity.class.getSimpleName();
@@ -46,10 +47,7 @@ public class MessageActivity extends BaseActivity {
     private List<Message> listMessages;
     private ListView listViewMessages;
 
-
-
     private User mUser;
-
     private Contact mContact;
     private boolean mIsClient;
 
@@ -57,6 +55,12 @@ public class MessageActivity extends BaseActivity {
     private BufferedReader mClientIn;
     private PrintWriter mClientOut;
 
+    private ServerSocket mServerSocketObj;
+    private Socket mServerSocket;
+    private BufferedReader mServerIn;
+    private PrintWriter mServerOut;
+
+    private String mMessageContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,8 +89,7 @@ public class MessageActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 // Sending message to web socket server
-
-
+                sendMessage(inputMsg.getText().toString().trim());
                 // Clearing the input filed once message was sent
                 inputMsg.setText("");
             }
@@ -100,7 +103,16 @@ public class MessageActivity extends BaseActivity {
     }
 
     private void startServerSocket() {
-
+        try {
+            mServerSocketObj = new ServerSocket(Utils.SOCKET_PORT);
+            mServerSocket = mServerSocketObj.accept();
+            mServerIn = new BufferedReader(new InputStreamReader(mServerSocket
+                    .getInputStream()));
+            mServerOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                    mServerSocket.getOutputStream())), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void startClientSocket() {
@@ -136,16 +148,32 @@ public class MessageActivity extends BaseActivity {
     }
 
     private void sendMessageFromServerToClient(String messageCipherText) {
-
+        Log.d(Utils.TAG,"sendMessageFromServerToClient:"+ messageCipherText);
+        if (mServerSocket!=null&&mServerSocket.isConnected()) {
+            if (!mServerSocket.isOutputShutdown()) {
+                mServerOut.println(messageCipherText);
+            }
+        }
     }
 
     private void sendMessageFromClientToServer(String messageCipherText) {
-
+        Log.d(Utils.TAG,"sendMessageFromClientToServer:"+ messageCipherText);
+        if (mClientSocket!=null&&mClientSocket.isConnected()) {
+            if (!mClientSocket.isOutputShutdown()) {
+                mClientOut.println(messageCipherText);
+            }
+        }
     }
 
-    private void addMessageToListView(Message message) {
-        mMessagesListAdapter.addMessage(message);
-        mMessagesListAdapter.notifyDataSetChanged();
+    private void addMessageToListView(final Message message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mMessagesListAdapter.addMessage(message);
+                mMessagesListAdapter.notifyDataSetChanged();
+                playBeep();
+            }
+        });
     }
 
     private Message constructMessage(String message) {
@@ -157,43 +185,6 @@ public class MessageActivity extends BaseActivity {
         return sendMessage;
     }
 
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-    }
-
-    /**
-     * Appending message to list view
-     */
-    private void appendMessage(final Message m) {
-        runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-                listMessages.add(m);
-
-                mMessagesListAdapter.notifyDataSetChanged();
-
-                // Playing device's notification
-                playBeep();
-            }
-        });
-    }
-
-    private void showToast(final String message) {
-
-        runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), message,
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-    }
 
     /**
      * Plays device's default notification sound
@@ -211,16 +202,69 @@ public class MessageActivity extends BaseActivity {
         }
     }
 
-    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Socket socket = null;
+                BufferedReader in = null;
+                if (mIsClient){
+                    socket = mClientSocket;
+                    in = mClientIn;
+                }else{
+                    socket = mServerSocket;
+                    in = mServerIn;
+                }
+                if (!socket.isClosed()) {
+                    if (socket.isConnected()) {
+                        if (!socket.isInputShutdown()) {
+                            if ((mMessageContent = in.readLine()) != null) {
+                                mMessageContent += "\n";
+                                receiveMessage(mMessageContent);
+                            } else {
+
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return new String(hexChars);
     }
 
+    private void receiveMessage(String mMessageContent) {
+        Log.d(Utils.TAG,"receive message,mMessageContent:"+ mMessageContent);
+        String messageContent = AES.decrypt(mContact.getMasterKey(), mMessageContent);
+        Log.d(Utils.TAG,"receive ,messagePlainText:"+ messageContent);
+        Gson gson = new Gson();
+        Message message = gson.fromJson(messageContent, Message.class);
+        addMessageToListView(message);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mIsClient) {
+            try {
+                mClientIn.close();
+                mClientOut.close();
+                mClientSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                mServerIn.close();
+                mServerOut.close();
+                mServerSocket.close();
+                mServerSocketObj.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 }
